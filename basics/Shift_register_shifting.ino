7cbcda98bb81cc2b932cbf03c0c9124e0a020936
rib44 <52131056+rib44@users.noreply.github.com>
2022-10-30 16:31:04 +0530

    shift register, uses bit shifting

---
+++ basics/Shift_register_shifting.ino
@@ -0,0 +1,28 @@
+int latchPin = 11;
+int clockPin = 9;
+int dataPin = 12; 
+int dt = 2250;
+
+byte myByte=0b10010011;
+byte tmp = myByte;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(latchPin, OUTPUT);
+pinMode(clockPin, OUTPUT);
+pinMode(dataPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, LSBFIRST, myByte);
+  digitalWrite(latchPin, HIGH);
+  Serial.println(myByte, BIN);  
+  myByte = (tmp*2) + (tmp/128);
+  tmp = myByte;
+  delay(dt);
+  
+
+}
